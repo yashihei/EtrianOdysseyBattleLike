34ecd716157de432b61c13215e4d0b0c6009f77d
yashihei <yashihei@gmail.com>
2022-06-18 09:12:42 +0900

    Fact HPが0未満にならない

---
+++ Tests/ActorTest.cs
@@ -0,0 +1,16 @@
+﻿using Etrain;
+using Xunit;
+
+namespace Tests;
+
+public class ActorTest
+{
+    [Fact]
+    public void HPが0未満にならない()
+    {
+        var actor = new Actor(0, "hoge", 100, 0, true);
+        actor.ApplyHpDamage(200);
+
+        Assert.False(actor.Hp < 0);
+    }
+}
