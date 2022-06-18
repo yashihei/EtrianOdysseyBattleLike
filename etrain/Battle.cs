bfe41e464b0fb8a78d2fcb75203fa2bed84e06ae
yashihei <yashihei@gmail.com>
2022-06-18 14:45:56 +0900

    Battleクラスのリファクタリング
    テストし易いように外から入力と進行を制御出来るようにした

--- etrain/Battle.cs
+++ etrain/Battle.cs
@@ -1,29 +1,35 @@
-﻿namespace Etrain;
+﻿using System.Diagnostics;
+
+namespace Etrain;
 
 public class Battle
 {
-    private int phase = 0;
-    private IEnumerable<Actor> actors = new Actor[] {
-        new(0, "player1", 100, 0, true),
-        new(1, "player2", 100, 0, true),
-        new(2, "enemy1", 100, 0, false),
-        new(3, "enemy2", 100, 0, false) };
+    private int phase = 1;
+    private IReadOnlyCollection<Actor> players;
+    private IReadOnlyCollection<Actor> enemies;
+    private List<Command> commands = new();
     private ActiveSkill naguru = new(0, "殴る", 10);
 
-    public bool RunPhase()
+    public void EnterActors(IEnumerable<Actor> actors)
     {
-        phase++;
+        players = actors.Where(actor => actor.IsPc).ToArray();
+        enemies = actors.Where(actor => !actor.IsPc).ToArray();
+    }
+
+    public void InputCommands(IEnumerable<Command> commands)
+    {
+        this.commands.AddRange(commands);
+    }
+
+    public void InputCommandsByConsole()
+    {
+        var aliveEnemies = enemies.Where(enemy => !enemy.IsDead()).ToArray();
+        var enemyTexts = aliveEnemies.Select((enemy, idx) => $"[{idx}] {enemy.Name} HP={enemy.Hp}").ToArray();
+
         Console.WriteLine($"=====================================");
         Console.WriteLine($"phase{phase}");
         Console.WriteLine($"=====================================");
 
-        var players = actors.Where(actor => actor.IsPc).ToArray();
-        var enemies = actors.Where(actor => !actor.IsPc).ToArray();
-        var aliveEnemies = enemies.Where(enemy => !enemy.IsDead()).ToArray();
-        var enemyTexts = aliveEnemies.Select((enemy, idx) => $"[{idx}] {enemy.Name} HP={enemy.Hp}").ToArray();
-        var commands = new List<Command>();
-
-        // コマンド入力
         foreach (var player in players)
         {
             Console.WriteLine("誰に攻撃しますか?");
@@ -32,8 +38,14 @@ public class Battle
             var command = new Command(player, aliveEnemies[targetIndex], naguru);
             commands.Add(command);
         }
+    }
 
-        // バトル進行
+    public void ProgressPhase()
+    {
+        phase++;
+
+        Debug.Assert(ValidateCommands(), "Invalid command!!");
+
         foreach (var command in commands)
         {
             var hit = command.Evaluate();
@@ -42,9 +54,25 @@ public class Battle
             Console.WriteLine(hit.ToString());
         }
 
-        if (enemies.All(enemy => enemy.IsDead()))
+        commands.Clear();
+    }
+
+    public bool IsEnd()
+    {
+        return enemies.All(enemy => enemy.IsDead());
+    }
+
+    private bool ValidateCommands()
+    {
+        if (commands.Count != players.Count)
         {
-            Console.WriteLine("バトルに勝利した！");
+            return false;
+        }
+
+        var commandTargetIds = commands.Select(command => command.Target.Id);
+        var enemyIds = enemies.Select(enemy => enemy.Id);
+        if (commandTargetIds.Except(enemyIds).Any())
+        {
             return false;
         }
 
--- etrain/Program.cs
+++ etrain/Program.cs
@@ -4,6 +4,19 @@ using Etrain;
 
 var battle = new Battle();
 
-while (battle.RunPhase())
+var actors = new Actor[] {
+    new(0, "player1", 100, 0, true),
+    new(1, "player2", 100, 0, true),
+    new(2, "enemy1", 100, 0, false),
+    new(3, "enemy2", 100, 0, false) };
+
+battle.EnterActors(actors);
+
+while (!battle.IsEnd())
 {
+    battle.InputCommandsByConsole();
+    battle.ProgressPhase();
 }
+
+// TODO: 敗北対応
+Console.WriteLine("バトルに勝利した！");
