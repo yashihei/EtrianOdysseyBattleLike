0d7299341f4405616af5fdb5ca9381ca08f7254f
yashihei <yashihei@gmail.com>
2022-06-26 19:39:36 +0900

    Hp回復を表すActを作成

---
+++ etrain/Act/HpHeal.cs
@@ -0,0 +1,25 @@
+﻿namespace Etrain;
+
+public class HpHeal : IAct
+{
+    public Actor Source { get; }
+    public Actor Target { get; }
+    public int HealValue { get; }
+
+    public HpHeal(Actor source, Actor target, int healValue)
+    {
+        Source = source;
+        Target = target;
+        HealValue = healValue;
+    }
+
+    public void Execute()
+    {
+        Target.ApplyHpHeal(HealValue);
+    }
+
+    public override string ToString()
+    {
+        return $"{Target.Name}は{HealValue}回復した！";
+    }
+}
